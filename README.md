b7e230ad9bdc0e5e9aeff26e515e339a843f72b4
Alexander Harvey Nitz <alex.nitz@ligo.org>
2019-03-18 16:50:15 +0100

    update files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pycbc_bench
 Some benchmark singularity images for pycbc / pycbc inference
+
+# build singularity image
+sudo singularity build pycbcb.img singularity
+
+# run pycbc inspiral
+singularity run --app inspiral pycbcb.img
--- run_inspiral.sh
+++ run_inspiral.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export NCORES=`getconf _NPROCESSORS_ONLN`
-
+echo "Using cores: ${NCORES}"
 for ((c=1; c<=$NCORES; c++ ))
 do
     /usr/bin/time -f '%e' \
@@ -40,7 +40,7 @@ do
     --gps-end-time 1126259846 \
     --output test.hdf \
     --approximant "SPAtmplt:mtotal<4" "IMRPhenomD:else" \
-    --bank-file bank.hdf 2> $c.log &
+    --bank-file /bank.hdf 2> $c.log &
 done
 wait $!
 sleep 10

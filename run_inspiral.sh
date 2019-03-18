b7f5486bf920f556fbbc55f118300c430f94b541
Alexander Harvey Nitz <alex.nitz@ligo.org>
2019-03-18 15:25:58 +0100

    initial commit

---
+++ run_inspiral.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+export NCORES=`getconf _NPROCESSORS_ONLN`
+
+for ((c=1; c<=$NCORES; c++ ))
+do
+    /usr/bin/time -f '%e' \
+    pycbc_inspiral \
+    --fake-strain-seed 0 \
+    --fake-strain aLIGOZeroDetLowPower \
+    --sample-rate 2048 \
+    --sgchisq-snr-threshold 6.0 \
+    --sgchisq-locations "mtotal>40:20-30,20-45,20-60,20-75,20-90,20-105,20-120" \
+    --segment-end-pad 16 \
+    --cluster-method window \
+    --low-frequency-cutoff 30 \
+    --pad-data 8 \
+    --cluster-window 1 \
+    --cluster-function symmetric \
+    --injection-window 4.5 \
+    --segment-start-pad 112 \
+    --psd-segment-stride 8 \
+    --psd-inverse-length 16 \
+    --filter-inj-only \
+    --psd-segment-length 16 \
+    --processing-scheme cpu:1 \
+    --fftw-measure-level 0 \
+    --snr-threshold 5.5 \
+    --segment-length 256 \
+    --autogating-width 0.25 \
+    --autogating-threshold 100 \
+    --autogating-cluster 0.5 \
+    --autogating-taper 0.25 \
+    --newsnr-threshold 5 \
+    --psd-estimation median \
+    --strain-high-pass 20 \
+    --order -1 \
+    --chisq-bins "1.75*(get_freq('fSEOBNRv2Peak',params.mass1,params.mass2,params.spin1z,params.spin2z)-60.)**0.5" \
+    --channel-name H1:LOSC-STRAIN \
+    --gps-start-time 1126259078 \
+    --gps-end-time 1126259846 \
+    --output test.hdf \
+    --approximant "SPAtmplt:mtotal<4" "IMRPhenomD:else" \
+    --bank-file bank.hdf 2> $c.log &
+done
+wait $!
+sleep 10
+export PROFTIME=`cat 1.log`
+bc <<< "$PROFTIME / $NCORES"
